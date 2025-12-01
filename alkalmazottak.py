561fde6f83ae07bda0b4707fe650af82f1f2273d
kala <kalae1337@gmail.com>
2025-12-01 16:09:47 +0100

    a

--- alkalmazottak.py
+++ alkalmazottak.py
@@ -1,3 +1,37 @@
-class Alkalmazottak:
-    def __init__(self, azonosito, nev, fizetes, ):
-        pass
+from osztaly import Alkalmazottak
+        
+
+class Work():
+    def __init__(self):
+        self.alk_lista = []
+
+    def betolt_alkalmazottak(self):
+        file = open("alkalmazottak.txt", "r", encoding="utf-8")
+        row =  file.readline()
+
+
+        while(row):
+            row = file.readline()
+            rowSp = row.split(";")  
+            if len(rowSp) > 1:
+                alkalmazott = Alkalmazottak(rowSp[0],rowSp[1],rowSp[2],rowSp[3])
+                self.alk_lista.append(alkalmazott)     
+        return self.alk_lista
+
+
+
+    def ment_alkalmazottakat(self):
+        for alk in self.alk_lista:
+            file = open("alkalmazottak_veg", "a", encoding="utf-8")
+            file.write(alk[0])
+            file.write(alk[1])
+            file.write(alk[2])
+            file.write(alk[3])
+        return "A fájlba írás megtörtént"
+
+    def controller(self):
+        readSuccess = self.betolt_alkalmazottak()
+        self.ment_alkalmazottakat()
+
+work = Work()
+work.controller()               
---
+++ osztaly.py
@@ -0,0 +1,6 @@
+class Alkalmazottak():
+    def __init__(self, azonosito, nev, fizetes, pozicio ):
+        self.azonosito = azonosito
+        self.nev = nev
+        self.fizetes = fizetes
+        self.pozicio = pozicio
